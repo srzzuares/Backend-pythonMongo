edc69b3c4142ce6cb6604e132adce3bd14c81793
sr <srzzuares@gmail.com>
2023-07-10 18:31:30 -0600

    cambios

---
+++ config/db.py
@@ -0,0 +1,2 @@
+from sqlalchemy import create_engine,MetaData
+engine = create_engine(meta_data)
---
+++ schemas/alumno.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+class UserModel(BaseModel):
+    matricula : int
+    nombre : str
+    apellidos : str
+    cuatrimestre : int
+    problema : float
