ecb5a387a1957d6dbe4792b0be9e8fb5823ce35c
sr <srzzuares@gmail.com>
2023-07-10 18:41:44 -0600

    code

--- config/db.py
+++ config/db.py
@@ -1,2 +1,7 @@
 from sqlalchemy import create_engine,MetaData
+from dotenv import load_dotenv
+
+load_dotenv()
+
 engine = create_engine(meta_data)
+meta=MetaData()
---
+++ models/alumno.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Table,Column
+
+from sqlalchemy.sql.sqltypes import Integer,String,Float
+from config.db import meta,engine
+
+alumnos = Table('alumnos',meta,
+                Column('matricula', Integer, primary_key=True),
+                Column('nombre', String(255)),
+                Column('apellidos', String(255)),
+                Column('cuatrimestre', Integer),
+                Column('promedio', Float))
+meta.create_table(engine)
+
