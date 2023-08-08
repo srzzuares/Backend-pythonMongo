bfca3226261002938b3c81dc01214873e752bed4
sr <srzzuares@gmail.com>
2023-08-07 19:04:59 -0600

    AngularFront

--- Frontend/Centro-Escolar/src/app/app.module.ts
+++ Frontend/Centro-Escolar/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormComponent } from './components/form/form.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
